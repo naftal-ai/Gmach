93b9cc55b00f11a878acc259e68bcd7d90d67bac
naftal-ai <144212558+naftal-ai@users.noreply.github.com>
2025-03-09 22:10:48 +0200

    db folder and customer model

---
+++ backend/app/models/customers.py
@@ -0,0 +1,19 @@
+from sqlalchemy import Column, Integer, Boolean, String, DateTime
+from sqlalchemy.sql import func
+from app.db.base import Base
+
+class Customer(Base):
+    __tablename__ = "customers"
+
+    id = Column(Integer, primary_key=True, index=True)
+    first_name = Column(String, index=True)
+    last_name = Column(String, index=True)
+    email = Column(String, unique=True, index=True)
+    phone = Column(String, index=True)
+    another_phone = Column(String, index=True)
+    address = Column(String, index=True)
+    is_approved = Column(Boolean, default=False)
+    created_at = Column(DateTime(timezone=True), server_default=func.now())
+    updated_at = Column(DateTime(timezone=True), onupdate=func.now())
+
+    
